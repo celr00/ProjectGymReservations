b3ef1a24f57016a3383c0a7a92a571099bcf7ca3
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-10-26 09:56:20 -0500

    Unimportant Changes

--- main.cpp
+++ main.cpp
@@ -6,5 +6,12 @@ using namespace std;
 
 int main(){
 
+    Hour fh(13,30);
+
+    Reservation num1 = Reservation("CL001", 1, fh, 30);
+
+    cout<<fh<<endl;
+    cout<<num1.getEndingHour()<<endl;
+
     return 0;
 }
