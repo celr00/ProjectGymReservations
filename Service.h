f33182302012ad0c7b5ba473d1f33ceffc847c90
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-10-26 11:28:22 -0500

    Create Service.h

---
+++ Service.h
@@ -0,0 +1,39 @@
+#ifndef ServiceClass
+#define ServiceClass
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Service{
+    public:
+        /*Constructors*/
+        Service();
+        Service(string cd, int mt, char t);
+        /*Sets and Gets*/
+        void setCode(string cd){code = cd;}
+        void setMaxTime(int mt){maxTime = mt;}
+        string getCode(){return code;}
+        int getMaxTime(){return maxTime;}
+        char getType(){return type;}
+        /*Methods*/
+        virtual void show() = 0;
+        virtual double getFinalCost() = 0;
+    protected:
+        string code;
+        int maxTime;
+        char type;
+};
+
+Service::Service(){
+    code = " ";
+    maxTime = 60;
+    type = ' ';
+}
+Service::Service(string cd, int mt, char t){
+    code = cd;
+    maxTime = mt;
+    type = t;
+}
+
+#endif
