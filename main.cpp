7cc1910aa704d229c9952d452a135b7eea34149e
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-10-27 18:07:58 -0500

    Unimportant changes

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 #include "Reservation.h"
+#include "Machine.h"
 
 using namespace std;
 
 int main(){
 
-    Hour fh(13,30);
+    Machine fm("CL001", 60, 'C', 15.5, true, "Some description.");
 
-    Reservation num1 = Reservation("CL001", 1, fh, 30);
-
-    cout<<fh<<endl;
-    cout<<num1.getEndingHour()<<endl;
+    fm.show();
 
     return 0;
 }
