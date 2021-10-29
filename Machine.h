1be1a59a9609ccc44e72dd8bbb00874df46125d3
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-10-29 18:14:05 -0500

    Machine - spanish to english

--- Machine.h
+++ Machine.h
@@ -8,30 +8,30 @@ class Machine:public Service{
         Machine();
         Machine(string cd, int mt, char t, double cst, bool ci, string desc);
         /*Sets and Gets*/
-        void setCostoX15min(double cst){costoX15min = cst;}
-        void setConInstructor(bool ci){conInstructor = ci;}
+        void setCostoX15min(double cst){costX15min = cst;}
+        void setWithInstructor(bool ci){withInstructor = ci;}
         void setDescripcion(string desc){descripcion = desc;}
-        double getCostoX15min(){return costoX15min;}
-        bool getConInstructor(){return conInstructor;}
+        double getCostoX15min(){return costX15min;}
+        bool getWithInstructor(){return withInstructor;}
         string getDescripcion(){return descripcion;}
         /*Methods*/
         void show();
         double getFinalCost(int t);
     private:
-        double costoX15min;
-        bool conInstructor;
+        double costX15min;
+        bool withInstructor;
         string descripcion;
 };
 
 /*Constructors*/
 Machine::Machine():Service(){
-    costoX15min = 0;
-    conInstructor = false;
+    costX15min = 0;
+    withInstructor = false;
     descripcion = "";
 }
 Machine::Machine(string cd, int mt, char t, double cst, bool ci, string desc):Service(cd, mt, t){
-    costoX15min = cst;
-    conInstructor = ci;
+    costX15min = cst;
+    withInstructor = ci;
     descripcion = desc;
 }
 
@@ -45,14 +45,13 @@ void Machine::show(){
     else if(type == 'B' or type == 'b')
         cout<<"Stacionary Bicycle"<<endl;
     cout<<"Description: "<<descripcion<<endl;
-    cout<<"$"<<costoX15min<<" every 15 minutes."<<endl;
-    if(conInstructor)
+    cout<<"$"<<costX15min<<" every 15 minutes."<<endl;
+    if(withInstructor)
         cout<<"It includes an instructor."<<endl;
     else
         cout<<"It does not include an instructor."<<endl;
     cout<<"*Max Time of rent: "<<maxTime<<endl<<endl;
-    
 }
 double Machine::getFinalCost(int t){
-    return (double)t/15*costoX15min;
+    return (double)t/15*costX15min;
 }
