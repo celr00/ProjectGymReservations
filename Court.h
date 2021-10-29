3e561eeb2c7d073ccec035f047052e5beb4d407a
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-10-29 18:12:23 -0500

    Court's methods

--- Court.h
+++ Court.h
@@ -14,8 +14,25 @@ class Court:public Service{
         double getCostXHour(){return costXHour;}
         int getPeopleCapacity(){return peopleCapacity;}
         string getSport(){return sport;}
+        /*Methods*/
+        void show();
+        double getFinalCost(int t);
     private:
         double costXHour;
         int peopleCapacity;
         string sport;
-};
+};void Court::show(){
+    cout<<code<<"| Court: ";
+    if(type == 'T' or type == 't')
+        cout<<"Tenis"<<endl;
+    else if(type == 'F' or type == 'f')
+        cout<<"Froton"<<endl;
+    else if(type == 'V' or type == 'v')
+        cout<<"Volley Ball"<<endl;
+    cout<<"People Capacity: "<<peopleCapacity<<endl;
+    cout<<"$"<<costXHour<<" every hour."<<endl;
+    cout<<"*Max Time of rent: "<<maxTime<<endl<<endl;
+}
+double Court::getFinalCost(int t){
+    return (double)t/60*costXHour;
+}
